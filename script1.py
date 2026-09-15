4f4de1ea682b0dd4085d71d3e952ceedbf2a1f99
Skuuzii <Devfordbusiness@gmail.com>
2026-09-15 15:49:10 -0400

    Fscript1.py

--- script1.py
+++ script1.py
@@ -5,15 +5,15 @@
 
 
 name: str = input("Please enter your name:")
-print("Hello"",""name")
+print("Hello""," + name)
 
-num1: str = input("Please enter the first variable:")
+num1: str = input(int("Please enter the first variable:"))
 # Use a breakpoint in the code line below to debug your script.
 
 opp: str = input("Order of operation?:* or /?")
 
-num2: str = input("Please enter the second variable:")
-ans: str = num1 + opp + num2
+num2 = input(int("Please enter the second variable:"))
+ans = eval(num1 + opp + num2)
 
 # Press the green button in the gutter to run the script.
-print("The answer to your query is:","ans")
+print("The answer to your query is:", ans)
