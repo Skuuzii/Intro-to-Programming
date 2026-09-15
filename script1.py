5fd8d4010eb0b26ac21dce32393421d21512636f
Skuuzii <Devfordbusiness@gmail.com>
2026-09-15 16:12:32 -0400

    VARIABLES ASSIGNMENT

--- script1.py
+++ script1.py
@@ -2,7 +2,7 @@ name = input("Please enter your name:")
 print("Hello, " + name)
 
 num1 = int(input("Please enter the first variable:"))
-opp = input("Order of operation?: * or /")
+opp = input("Order of operation?: *, %, +, -, /")
 num2 = int(input("Please enter the second variable:"))
 ans = eval(str(num1) + opp + str(num2))
 
