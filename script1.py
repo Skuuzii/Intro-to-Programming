cdf5cce2420113c9dec3fed198bde9d0849e8f63
Skuuzii <Devfordbusiness@gmail.com>
2026-09-15 15:44:05 -0400

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool window
+
+
+name: str = input("Please enter your name:")
+print("Hello"",""name")
+
+num1: str = input("Please enter the first variable:")
+# Use a breakpoint in the code line below to debug your script.
+
+opp: str = input("Order of operation?:* or /?")
+
+num2: str = input("Please enter the second variable:")
+ans: str = num1 + opp + num2
+
+# Press the green button in the gutter to run the script.
+print("The answer to your query is:","ans")
