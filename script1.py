b7943d3c58ef3826d72eedc4df8c6f4131ff01c5
Skuuzii <Devfordbusiness@gmail.com>
2026-09-15 15:56:12 -0400

    SCRIPT 1 POLISHED AGN

--- script1.py
+++ script1.py
@@ -1,19 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool window
-
-
 name = input("Please enter your name:")
-print("Hello""," + name)
+print("Hello, " + name)
 
-num1 = input(int("Please enter the first variable:"))
-# Use a breakpoint in the code line below to debug your script.
-
-opp = input("Order of operation?:* or /?")
-
-num2 = input(int("Please enter the second variable:"))
+num1 = int(input("Please enter the first variable:"))
+opp = input("Order of operation?: * or /")
+num2 = int(input("Please enter the second variable:"))
 ans = eval(num1 + opp + num2)
 
-# Press the green button in the gutter to run the script.
 print("The answer to your query is:", ans)
